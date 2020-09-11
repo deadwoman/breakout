806dc3d81bdcffaa005699624e13f0bc99ffc08a
Emily Deadman <emilydeadman@Emilys-MacBook-Pro.local>
2020-09-11 16:01:01 -0400

    major styling updates. changed game colors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const rulesBtn = document.getElementById('rules-btn');
-const closeBtn = document.getElementById('close-btn');
-const rules = document.getElementById('rules');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 let score = 0;
 
@@ -16,7 +16,7 @@ const ball = {
   size: 10,
   speed: 4,
   dx: 4,
-  dy: -4
+  dy: -4,
 };
 
 // Create paddle props
@@ -26,7 +26,7 @@ const paddle = {
   w: 80,
   h: 10,
   speed: 8,
-  dx: 0
+  dx: 0,
 };
 
 // Create brick props
@@ -36,7 +36,7 @@ const brickInfo = {
   padding: 10,
   offsetX: 45,
   offsetY: 60,
-  visible: true
+  visible: true,
 };
 
 // Create bricks
@@ -54,7 +54,7 @@ for (let i = 0; i < brickRowCount; i++) {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
-  ctx.fillStyle = '#0095dd';
+  ctx.fillStyle = "#663a82";
   ctx.fill();
   ctx.closePath();
 }
@@ -63,24 +63,24 @@ function drawBall() {
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
-  ctx.fillStyle = '#0095dd';
+  ctx.fillStyle = "#663a82";
   ctx.fill();
   ctx.closePath();
 }
 
 // Draw score oon canvas
 function drawScore() {
-  ctx.font = '20px Arial';
+  ctx.font = "15px Andale Mono";
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
 
 // Draw bricks on canvas
 function drawBricks() {
-  bricks.forEach(column => {
-    column.forEach(brick => {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
       ctx.beginPath();
       ctx.rect(brick.x, brick.y, brick.w, brick.h);
-      ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+      ctx.fillStyle = brick.visible ? "#bf94e4" : "transparent";
       ctx.fill();
       ctx.closePath();
     });
@@ -128,8 +128,8 @@ function moveBall() {
   }
 
   // Brick collision
-  bricks.forEach(column => {
-    column.forEach(brick => {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
       if (brick.visible) {
         if (
           ball.x - ball.size > brick.x && // left brick side check
@@ -164,8 +164,8 @@ function increaseScore() {
 
 // Make all bricks appear
 function showAllBricks() {
-  bricks.forEach(column => {
-    column.forEach(brick => (brick.visible = true));
+  bricks.forEach((column) => {
+    column.forEach((brick) => (brick.visible = true));
   });
 }
 
@@ -195,9 +195,9 @@ update();
 
 // Keydown event
 function keyDown(e) {
-  if (e.key === 'Right' || e.key === 'ArrowRight') {
+  if (e.key === "Right" || e.key === "ArrowRight") {
     paddle.dx = paddle.speed;
-  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
     paddle.dx = -paddle.speed;
   }
 }
@@ -205,19 +205,19 @@ function keyDown(e) {
 // Keyup event
 function keyUp(e) {
   if (
-    e.key === 'Right' ||
-    e.key === 'ArrowRight' ||
-    e.key === 'Left' ||
-    e.key === 'ArrowLeft'
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
   ) {
     paddle.dx = 0;
   }
 }
 
 // Keyboard event handlers
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 // Rules and close event handlers
-rulesBtn.addEventListener('click', () => rules.classList.add('show'));
-closeBtn.addEventListener('click', () => rules.classList.remove('show'));
+rulesBtn.addEventListener("click", () => rules.classList.add("show"));
+closeBtn.addEventListener("click", () => rules.classList.remove("show"));
